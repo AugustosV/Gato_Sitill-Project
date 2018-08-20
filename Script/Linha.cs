24e3c0b7230f14c6b820d53a44c2daee643102c2
AugustosV <42479673+AugustosV@users.noreply.github.com>
2018-08-20 19:29:23 -0300

    Update Linha.cs

--- Script/Linha.cs
+++ Script/Linha.cs
@@ -8,6 +8,7 @@ public class Linha : MonoBehaviour
 	// Use this for initialization
 	public void posicaoEmx()
 	{
+		//sorteia as posiçoes dos objetos apos serem instanciados 
 		float sorteiap = Random.Range(-0.25f,0.28f);
 		if(sorteiap < 0)
 			transform.position = new Vector3 (-0.2f, transform.position.y, transform.position.z);
